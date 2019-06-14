52acfa69cd5f2cb07367761879004e77cbd088fd
Marcus Puckett <mpuckett259@gmail.com>
2019-06-14 16:02:55 -0500

    Add xMove-VMs Script
    Porting over William Lam's xMove-VM script with the updates I've made detailed, adding support for:
    
     - list of NIC network names instead of CSV string
     - changing from clear text password to credentials
     - adding source and destination datacenter inputs to distinguish between different vCenter objects with the same names, e.g. network names and datastores

---
+++ xMove-VMs.ps1
@@ -0,0 +1,198 @@
+<#
+.SYNOPSIS
+   This script demonstrates an xVC-vMotion where a running Virtual Machine
+   is live migrated between two vCenter Servers which are NOT part of the
+   same SSO Domain which is only available using the vSphere 6.0 API.
+   This script also supports live migrating a running Virtual Machine between
+   two vCenter Servers that ARE part of the same SSO Domain (aka Enhanced Linked Mode)
+   This script also supports migrating VMs connected to both a VSS/VDS as well as having multiple vNICs
+   This script also supports migrating to/from VMware Cloud on AWS (VMC)
+.NOTES
+   File Name  : xMove-VM.ps1
+   Author     : William Lam - @lamw
+   Version    : 1.0
+   Updated by  : Askar Kopbayev - @akopbayev
+   Version     : 1.1
+   Description : The script allows to run compute-only xVC-vMotion when the source VM has multiple disks on differnet datastores.
+   Updated by  : William Lam - @lamw
+   Version     : 1.2
+   Description : Added additional parameters to be able to perform cold migration to/from VMware Cloud on AWS (VMC)
+                 -ResourcePool
+                 -uppercaseuuid
+   Updated by  : Marcus Puckett
+   Version     : 1.3
+   Description : Added datacenter awareness, removed dependency on plain text usernames and passwords, updated vmnetworks to 
+                 accept a list of strings
+.LINK
+    http://www.virtuallyghetto.com/2016/05/automating-cross-vcenter-vmotion-xvc-vmotion-between-the-same-different-sso-domain.html
+.LINK
+   https://github.com/lamw
+.INPUTS
+   sourcevc, destvc, vm, sourcedatacenter, destinationdatacenter, switchtype, switch,
+   cluster, resourcepool, datastore, vmhost, vmnetworks, $xvctype, $uppercaseuuid
+.OUTPUTS
+   Console output
+#>
+
+Function xMove-VM {
+    param(
+    [Parameter(
+        Position=0,
+        Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)
+    ]
+    [VMware.VimAutomation.ViCore.Util10.VersionedObjectImpl]$sourcevc,
+    [VMware.VimAutomation.ViCore.Util10.VersionedObjectImpl]$destvc,
+    [pscredential]$destVCCred,
+    [String]$vm,
+    [String]$sourcedatacenter,
+    [String]$destinationdatacenter,
+    [String]$switchtype,
+    [String]$switch,
+    [String]$sourceswitch,
+    [String]$cluster,
+    [String]$resourcepool,
+    [String]$datastore,
+    [String]$vmhost,
+    [String[]]$vmnetworks,
+    [Int]$xvctype,
+    [Boolean]$uppercaseuuid
+    )
+
+    # Retrieve Source VC SSL Thumbprint
+    $vcurl = "https://" + $destVC
+add-type @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+            public class IDontCarePolicy : ICertificatePolicy {
+            public IDontCarePolicy() {}
+            public bool CheckValidationResult(
+                ServicePoint sPoint, X509Certificate cert,
+                WebRequest wRequest, int certProb) {
+                return true;
+            }
+        }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy
+    # Need to do simple GET connection for this method to work
+    Invoke-RestMethod -Uri $VCURL -Method Get | Out-Null
+
+    $endpoint_request = [System.Net.Webrequest]::Create("$vcurl")
+    # Get Thumbprint + add colons for a valid Thumbprint
+    $destVCThumbprint = ($endpoint_request.ServicePoint.Certificate.GetCertHashString()) -replace '(..(?!$))','$1:'
+
+    # Source VM to migrate
+    $vm_view = Get-View (Get-VM -Server $sourcevc -Name $vm -Location $sourcedatacenter) -Property Config.Hardware.Device
+
+    # Dest Datastore to migrate VM to
+    $datastore_view = (Get-Datastore -Server $destVCConn -Name $datastore -Location $destinationdatacenter)
+
+    # Dest Cluster/ResourcePool to migrate VM to
+    if($cluster) {
+        $cluster_view = (Get-Cluster -Server $destVCConn -Name $cluster -Location $destinationdatacenter)
+        $resource = $cluster_view.ExtensionData.resourcePool
+    } else {
+        $rp_view = (Get-ResourcePool -Server $destVCConn -Name $resourcepool -Location $destinationdatacenter)
+        $resource = $rp_view.ExtensionData.MoRef
+    }
+
+    # Dest ESXi host to migrate VM to
+    $vmhost_view = (Get-VMHost -Server $destVCConn -Name $vmhost)
+
+    # Find all Etherenet Devices for given VM which
+    # we will need to change its network at the destination
+    $vmNetworkAdapters = @()
+    $devices = $vm_view.Config.Hardware.Device
+    foreach ($device in $devices) {
+        if($device -is [VMware.Vim.VirtualEthernetCard]) {
+            $vmNetworkAdapters += $device
+        }
+    }
+
+    # Relocate Spec for Migration
+    $spec = New-Object VMware.Vim.VirtualMachineRelocateSpec
+    $spec.datastore = $datastore_view.Id
+    $spec.host = $vmhost_view.Id
+    $spec.pool = $resource
+
+    # Relocate Spec Disk Locator
+    if($xvctype -eq 1){
+        $HDs = Get-VM -Server $sourcevc -Name $vm -Location $sourcedatacenter | Get-HardDisk
+        $HDs | %{
+            $disk = New-Object VMware.Vim.VirtualMachineRelocateSpecDiskLocator
+            $disk.diskId = $_.Extensiondata.Key
+            $SourceDS = $_.FileName.Split("]")[0].TrimStart("[")
+            $DestDS = Get-Datastore -Server $destvc -name $sourceDS -Location $destinationdatacenter
+            $disk.Datastore = $DestDS.ID
+            $spec.disk += $disk
+        }
+    }
+
+    # Service Locator for the destination vCenter Server
+    # regardless if its within same SSO Domain or not
+    $service = New-Object VMware.Vim.ServiceLocator
+    $credential = New-Object VMware.Vim.ServiceLocatorNamePassword
+    $credential.username = $destVCCred.GetNetworkCredential().username
+    $credential.password = $destVCCred.GetNetworkCredential().password
+    $service.credential = $credential
+    # For some xVC-vMotion, VC's InstanceUUID must be in all caps
+    # Haven't figured out why, but this flag would allow user to toggle (default=false)
+    if($uppercaseuuid) {
+        $service.instanceUuid = $destVCConn.InstanceUuid
+    } else {
+        $service.instanceUuid = ($destVCConn.InstanceUuid).ToUpper()
+    }
+    $service.sslThumbprint = $destVCThumbprint
+    $service.url = "https://$destVC"
+    $spec.service = $service
+
+    # Create VM spec depending if destination networking
+    # is using Distributed Virtual Switch (VDS) or
+    # is using Virtual Standard Switch (VSS)
+    $count = 0
+    if($switchtype -eq "vds") {
+        foreach ($vmNetworkAdapter in $vmNetworkAdapters) {
+            # New VM Network to assign vNIC
+            $vmnetworkname = $vmnetworks[$count]
+
+            # Extract Distributed Portgroup required info
+            $dvs = Get-VDSwitch -Name $switch -Location $destinationdatacenter
+            $dvpg = Get-VDPortgroup -Server $destvc -Name $vmnetworkname -VDSwitch $dvs
+            $vds_uuid = (Get-View $dvpg.ExtensionData.Config.DistributedVirtualSwitch).Uuid
+            $dvpg_key = $dvpg.ExtensionData.Config.key
+
+            # Device Change spec for VSS portgroup
+            $dev = New-Object VMware.Vim.VirtualDeviceConfigSpec
+            $dev.Operation = "edit"
+            $dev.Device = $vmNetworkAdapter
+            $dev.device.Backing = New-Object VMware.Vim.VirtualEthernetCardDistributedVirtualPortBackingInfo
+            $dev.device.backing.port = New-Object VMware.Vim.DistributedVirtualSwitchPortConnection
+            $dev.device.backing.port.switchUuid = $vds_uuid
+            $dev.device.backing.port.portgroupKey = $dvpg_key
+            $spec.DeviceChange += $dev
+            $count++
+        }
+    } else {
+        foreach ($vmNetworkAdapter in $vmNetworkAdapters) {
+            # New VM Network to assign vNIC
+            $vmnetworkname = $vmnetworks[$count]
+
+            # Device Change spec for VSS portgroup
+            $dev = New-Object VMware.Vim.VirtualDeviceConfigSpec
+            $dev.Operation = "edit"
+            $dev.Device = $vmNetworkAdapter
+            $dev.device.backing = New-Object VMware.Vim.VirtualEthernetCardNetworkBackingInfo
+            $dev.device.backing.deviceName = $vmnetworkname
+            $spec.DeviceChange += $dev
+            $count++
+        }
+    }
+
+    Write-Host "`nMigrating $vmname from $sourceVC to $destVC ...`n"
+
+    # Issue Cross VC-vMotion
+    $task = $vm_view.RelocateVM_Task($spec,"defaultPriority")
+    $task1 = Get-Task -Id ("Task-$($task.value)")
+    $task1 | Wait-Task
+}
