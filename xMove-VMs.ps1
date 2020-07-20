32171cd3aa50eb67b9589472ae21d9e66e519de4
Marcus Puckett <mpuckett259@gmail.com>
2020-07-20 13:50:51 -0700

    sync-changes

--- xMove-VMs.ps1
+++ xMove-VMs.ps1
@@ -19,16 +19,12 @@
    Description : Added additional parameters to be able to perform cold migration to/from VMware Cloud on AWS (VMC)
                  -ResourcePool
                  -uppercaseuuid
-   Updated by  : Marcus Puckett
-   Version     : 1.3
-   Description : Added datacenter awareness, removed dependency on plain text usernames and passwords, updated vmnetworks to 
-                 accept a list of strings
 .LINK
     http://www.virtuallyghetto.com/2016/05/automating-cross-vcenter-vmotion-xvc-vmotion-between-the-same-different-sso-domain.html
 .LINK
    https://github.com/lamw
 .INPUTS
-   sourcevc, destvc, vm, sourcedatacenter, destinationdatacenter, switchtype, switch,
+   sourceVCConnection, destVCConnection, vm, switchtype, switch,
    cluster, resourcepool, datastore, vmhost, vmnetworks, $xvctype, $uppercaseuuid
 .OUTPUTS
    Console output
@@ -44,7 +40,6 @@ Function xMove-VM {
     ]
     [VMware.VimAutomation.ViCore.Util10.VersionedObjectImpl]$sourcevc,
     [VMware.VimAutomation.ViCore.Util10.VersionedObjectImpl]$destvc,
-    [pscredential]$destVCCred,
     [String]$vm,
     [String]$sourcedatacenter,
     [String]$destinationdatacenter,
@@ -133,8 +128,8 @@ add-type @"
     # regardless if its within same SSO Domain or not
     $service = New-Object VMware.Vim.ServiceLocator
     $credential = New-Object VMware.Vim.ServiceLocatorNamePassword
-    $credential.username = $destVCCred.GetNetworkCredential().username
-    $credential.password = $destVCCred.GetNetworkCredential().password
+    $credential.username = $destVCusername
+    $credential.password = $destVCpassword
     $service.credential = $credential
     # For some xVC-vMotion, VC's InstanceUUID must be in all caps
     # Haven't figured out why, but this flag would allow user to toggle (default=false)
@@ -195,4 +190,225 @@ add-type @"
     $task = $vm_view.RelocateVM_Task($spec,"defaultPriority")
     $task1 = Get-Task -Id ("Task-$($task.value)")
     $task1 | Wait-Task
+}
+ 
+function Get-NewVMHost {
+    Param (
+        [parameter(Mandatory=$true)]
+        [String]
+        $targetCluster
+    )
+ 
+    $vcCluster = Get-Cluster -Name $targetCluster -Server $destVC
+    $targetVMHost = get-vmhost -location $vcCluster | Sort-Object -Property @{Expression = { $_.ExtensionData.Vm.count} } | Select-Object -First 1
+    return $targetVMHost
+}
+
+function xMove-NOAVMs {
+    Param(
+        [parameter(Mandatory=$false)]
+        [String[]]
+        $vmNameList,
+        [parameter(Mandatory=$true)]
+        [String]
+        $sourceVC,
+        [parameter(Mandatory=$true)]
+        [PSCredential]
+        $sourceVCCred,
+        [parameter(Mandatory=$true)]
+        [String]
+        $destVC,
+        [parameter(Mandatory=$true)]
+        [PSCredential]
+        $destVCCred,
+        [parameter(Mandatory=$false)]
+        [bool]
+        $dryRun=$true,
+        [parameter(Mandatory=$true)]
+        [String]
+        $statusLogPath
+    )
+
+    # Get source and destination vCenter connections from global parameters
+    $sourceVCConn = $global:defaultviservers | Where-Object { $_.Name -eq $sourceVC }
+    $destVCConn = $global:defaultviservers | Where-Object { $_.Name -eq $destVC }
+
+    ## Setting clear text username/password info necessary for the xMove-VM function
+    $destVCusername = $destVCCred.GetNetworkCredential().username
+    $destVCpassword = $destVCCred.GetNetworkCredential().password
+    $sourceVCusername = $sourceVCCred.GetNetworkCredential().username
+    $sourceVCpassword = $sourceVCCred.GetNetworkCredential().password
+
+    # Throw error if you are not connected to the specified vCenter Servers
+    if(!($sourceVCConn) -or !($destVCConn)){
+        Write-Host "You are not connected to one of the specified vCenter servers"
+        Write-Host "Source vCenter = $($sourceVCConn.Name)"
+        Write-Host "Destination vCenter = $($destVCConn.Name)"
+        Write-Host "Please connect to which ever vCenter setting above is blank, or both if they are both blank, and try again."
+        ThrowError "Exiting script"
+    }
+
+    # Looping through VMs selected and initiating the transfer
+    $vmActionList = @()
+    foreach($vmName in $vmNameList){
+        # Store current number of errors in $error variable
+        $errorCount = $error.count
+        
+        try{
+            # Pulling necessary information and translating names
+            $sourceVMObject = Get-VM -Name $vmName -Server $sourceVC
+            $sourceDC = Get-Datacenter -VM $sourceVMObject -Server $sourceVC
+            $destDCName = Get-Datacenter -Name $sourceDC.Name -Server $destVC
+            $sourceCluster = Get-Cluster -VM $sourceVMObject -Server $sourceVC
+            $targetClusterName = Get-Cluster -Name $sourceCluster.Name
+
+            # Setting vSphere Switch details
+            $switchtype = "vds"
+            $switchname = ($destDCName + "-dvSwitch01")
+            $sourceswitch = Get-VDSwitch -VM $sourceVMObject
+
+            # Setting VM Folder name and Resource Pool name
+            $vmparentfolder = $sourceVMObject.Folder.Parent.Name
+            $vmfolder = $sourceVMObject.Folder.Name
+            $resourcepool = "Resources"
+
+            # Getting correct VM Folder info
+            $vmfolderparentlist = @()
+            $sourceVmParentFolderObj = $sourceVMObject.Folder.Parent
+            while($sourceVmParentFolderObj.Name -ne "vm" -and $sourceVmParentFolderObj){
+                $vmfolderparentlist += $sourceVmParentFolderObj
+                $sourceVmParentFolderObj = $sourceVmParentFolderObj.Parent
+            }
+            [array]::Reverse($vmfolderparentlist)
+            $rootVMFolder = Get-Folder -Location $destDCName -Type VM -Name "vm" -Server $destVC
+            if($vmfolderparentlist.count -ge 1){
+                $i = 1
+                $vmfolderplaceholder = Get-Folder -Location $rootVMFolder -Type VM -Name $vmfolderparentlist[0] -Server $destVC -NoRecursion
+                while($i -lt $vmfolderparentlist.count){
+                    $vmfolderplaceholder = Get-Folder -Location $vmfolderplaceholder -Type VM -Name $vmfolderparentlist[$i] -Server $destVC -NoRecursion
+                    $i++
+                }
+                $vmFolderObj = Get-Folder -Location $vmfolderplaceholder -Type VM -Name $vmfolder -Server $destVC -NoRecursion
+            }
+            else {
+                $vmFolderObj = Get-Folder -Location $rootVMFolder -Type VM -Name $vmfolder -Server $destVC -NoRecursion
+            }
+
+            # Getting a VM Host to migrate the VM to
+            $vmhostname = (Get-NewVMHost -targetCluster $targetClusterName).Name
+
+            # Getting the datastore name, this is required for the migration but the VM will not move datastores
+            $datastorename = (Get-Datastore -Id $sourceVMObject.DatastoreIdList[0] -Server $sourceVC).Name
+
+            # Retrieving all VM NIC networks and creating an ordered list
+            $vmnetworks = (Get-NetworkAdapter -VM $sourceVMObject | Select-Object NetworkName).NetworkName
+
+            # Setting migration parameters as necessary, do not change
+            $computeXVC = 1
+            $UppercaseUUID = $true
+        }
+        catch{
+            Write-Host "Error getting VM information for migration"
+            $vmAction = @{
+                sourceVC = "$sourceVC"
+                destVC = "$destVC"
+                VMName = "$vmname"
+                SourceDC = "$($sourceDC.Name)"
+                DestinationDC = "$destDCName"
+                VMFolder = "$vmfolder"
+                switchType = "$switchtype"
+                resourcePool = "$resourcepool"
+                vmHost = "$vmhostname"
+                datastore = "$datastorename"
+                vmnetworks = $vmnetworks
+                computeXVC = "$computeXVC"
+                uppercaseuuid = "$UppercaseUUID"
+                moveStatus = "Failed to retrieve VM information"
+                errorMessage = $error[0]
+            }
+        }
+
+        if($dryRun -eq $false){
+            # Executing the xMove function
+            $vmAction = @{
+                sourceVC = "$sourceVC"
+                destVC = "$destVC"
+                VMName = "$vmname"
+                SourceDC = "$($sourceDC.Name)"
+                DestinationDC = "$destDCName"
+                VMFolder = "$vmfolder"
+                switchType = "$switchtype"
+                resourcePool = "$resourcepool"
+                vmHost = "$vmhostname"
+                datastore = "$datastorename"
+                vmnetworks = $vmnetworks
+                computeXVC = "$computeXVC"
+                uppercaseuuid = "$UppercaseUUID"
+                moveStatus = "Successful"
+                errorMessage = "None"
+            }
+            try {
+                $xMoveInputs = @{
+                    sourcevc = $sourceVCConn
+                    destvc = $destVCConn
+                    VM = $vmName
+                    sourcedatacenter = $sourceDC
+                    destinationdatacenter = $destDCName
+                    switchtype = $switchtype
+                    switch = $switchname
+                    sourceswitch = $sourceswitch.Name
+                    vmhost = $vmhostname
+                    cluster = $targetClusterName
+                    resourcepool = $resourcepool
+                    vmnetworks = $vmnetworks
+                    datastore = $datastorename
+                    xvcType = $computeXVC
+                    uppercaseuuid = $UppercaseUUID
+                }
+                xMove-VM @xMoveInputs
+                $destVMObj = Get-VM -Name $vmName -Server $destVC
+                if($vmfolder -ne "vm"){
+                    try {
+                        Move-VM -Server $destVC -VM $destVMObj -Destination $vmhostname -InventoryLocation $vmFolderObj | Out-Null
+                    }
+                    catch {
+                        Write-Host "Error moving VM $vmname to folder $vmfolder"
+                        $vmAction.folderMoveStatus = "Failed"
+                        $vmAction.folderMoveError = $error[0]
+                    }
+                }
+                Write-Host "Successfully migrated VM $($vmname)"
+            }
+            catch{
+                Write-Host "Error moving VM $vmname"
+                $vmAction.moveStatus = "Failed"
+                $vmAction.errorMessage = $error[0]
+            }
+        }
+        else{
+            # Set dry run variables if there are no errors
+            Write-Host "Dry Run enabled, not migrating VM $($vmname), logging data"
+            if($error.count -eq $errorCount){
+                $vmAction = @{
+                    sourceVC = "$sourceVC"
+                    destVC = "$destVC"
+                    VMName = "$vmname"
+                    SourceDC = "$($sourceDC.Name)"
+                    DestinationDC = "$destDCName"
+                    VMFolder = "$vmfolder"
+                    switchType = "$switchtype"
+                    resourcePool = "$resourcepool"
+                    vmHost = "$vmhostname"
+                    datastore = "$datastorename"
+                    vmnetworks = $vmnetworks
+                    computeXVC = "$computeXVC"
+                    uppercaseuuid = "$UppercaseUUID"
+                    moveStatus = "dryRun"
+                    errorMessage = "None"
+                }
+            }
+        }
+        $vmActionList += $vmAction
+    }
+    ConvertTo-Json -InputObject $vmActionList | Add-Content -Path $statusLogPath
 }
